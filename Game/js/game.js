ce588798279a93c0988c80a7f9e9aeba374ad446
Gabe_Bruh_AR <alan.burt@mackillop.act.edu.au>
2019-10-28 13:31:08 +1100

    got rid of things
    nothing really important

--- Game/js/game.js
+++ Game/js/game.js
@@ -9,10 +9,6 @@ game.state.add('gameover', gameoverState);
 
 game.global = {
 
-  variables
-  var background
-  var player
-  var
    // set variables that travel between the states here
    // eg score = 0;
    // access using game.global
