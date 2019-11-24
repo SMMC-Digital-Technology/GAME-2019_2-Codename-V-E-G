06b681034fe8cf9f84badac5f71551f0c91ae0d2
Gabe_Bruh-AR <alan.burt@mackillop.act.edu.au>
2019-11-24 19:45:40 +1100

    updated menu.js
    changed menu button code

--- Game/js/menu.js
+++ Game/js/menu.js
@@ -6,18 +6,13 @@ var menuState = {
    create: function() {
      title = game.add.sprite(400, 300, 'title')
 
-     menubutton = game.add.button(game.world.centerX, game.world.centerY, 'button');
+     var menubutton = game.add.button(game.world.centerX, game.world.centerY, 'button');
     menubutton.anchor.setTo(0.5,0.5);
     menubutton.onInputUp.add(this.startGame);
     if (menubutton.onInputUp){
       game.state.start('level')
 
       // create the scene
-      startGame: function() {
-          game.state.start('level')
-        }
-
-     };
 
    // this is how you write a function
    // note the comma after the } above
