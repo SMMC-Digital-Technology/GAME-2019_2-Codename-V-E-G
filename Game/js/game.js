c2ed0d990115f4031ff537e2166c54d0b81526e8
Gabe_Bruh-AR <alan.burt@mackillop.act.edu.au>
2019-11-25 22:12:36 +1100

    update
    changed assets button and added ne w code

--- Game/js/game.js
+++ Game/js/game.js
@@ -8,10 +8,6 @@ game.state.add('gameover', gameoverState);
 //game.state.add('endscreen', endState);
 
 game.global = {
-  score = 0
-};
-
-
    // set variables that travel between the states here
    // eg score = 0;
    // access using game.global
--- Game/js/gameover.js
+++ Game/js/gameover.js
@@ -9,12 +9,9 @@ var gameoverState = {
       //add button
       retrybutton = game.add.button(window.innerWidth / 2.3, game.world.centerY, 'retrybutton', actionOnClick, this, 2, 1, 0);
       function actionOnClick(){
-        game.state.start('menu');
-      //retrybutton = game.add.button(game.world.centerX, game.world.centerY, 'retrybutton');
-      //retrybutton.anchor.setTo(0.5,0.5);
-      //retrybutton.onInputUp.add(this.buttonclick);
-         //}
-   //},
+        game.state.start('menu')
+      }
 
-   }
-};
+    }
+
+ };
